373b732d5ca957daca0ef83c458446502a223124
Atıl Samancıoğlu <samancioglu.atil@gmail.com>
2017-02-12 20:19:10 +0300

    final version

--- Landmark Image Book/ImageViewController.swift
+++ Landmark Image Book/ImageViewController.swift
@@ -10,26 +10,23 @@ import UIKit
 
 class ImageViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+    var selectedLandmarkName = ""
+    var selectedLandmarkImage = UIImage()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+        imageView.image = selectedLandmarkImage
+        nameLabel.text = selectedLandmarkName
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
     }
-    */
+
+ 
 
 }
--- Landmark Image Book/ViewController.swift
+++ Landmark Image Book/ViewController.swift
@@ -8,18 +8,86 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var landmarkNamesArray = [String]()
+    var landmarkImageArray = [UIImage]()
+    
+    var chosenLandmarkName = ""
+    var chosenLandmarkImage = UIImage()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        //tableview setup
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        
+        //example array creation
+        
+        landmarkNamesArray.append("Colleseum")
+        landmarkNamesArray.append("Great Wall")
+        landmarkNamesArray.append("Kremlin")
+        landmarkNamesArray.append("Stonhenge")
+        landmarkNamesArray.append("Taj Mahal")
+        
+        landmarkImageArray.append(UIImage(named: "colleseum.jpeg")!)
+        landmarkImageArray.append(UIImage(named: "greatwall.jpeg")!)
+        landmarkImageArray.append(UIImage(named: "kremlin.png")!)
+        landmarkImageArray.append(UIImage(named: "stonehenge.jpeg")!)
+        landmarkImageArray.append(UIImage(named: "tajmahal.jpeg")!)
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return landmarkNamesArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete {
+            
+            landmarkNamesArray.remove(at: indexPath.row)
+            landmarkImageArray.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: UITableViewRowAnimation.fade)
+            
+        }
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        
+        if segue.identifier == "toImageVCSegue" {
+            let destinationVC = segue.destination as! ImageViewController
+            destinationVC.selectedLandmarkName = chosenLandmarkName
+            destinationVC.selectedLandmarkImage = chosenLandmarkImage
+        }
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        self.chosenLandmarkName = landmarkNamesArray[indexPath.row]
+        self.chosenLandmarkImage = landmarkImageArray[indexPath.row]
+        performSegue(withIdentifier: "toImageVCSegue", sender: nil)
+        
+        
     }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        cell.textLabel?.text = landmarkNamesArray[indexPath.row]
+        
+        return cell
+    }
 
 }
 
